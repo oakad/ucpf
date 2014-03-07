058a7c6061ef5eae42443186cfe6ac4ceef2ab8c
Alex Dubov <oakad@yahoo.com>
2014-03-07 19:27:18 +1100

    yesod::base86: redo decoding to use smaller lookup tables
    Signed-off-by: Alex Dubov <oakad@yahoo.com>

--- yesod/test/base86.cpp
+++ yesod/test/base86.cpp
@@ -15,7 +15,9 @@ namespace ucpf { namespace yesod {
 
 BOOST_AUTO_TEST_CASE(base85_0)
 {
-	char const *in("HelloWorld"), *in_p(in);
+	unsigned char const *in(reinterpret_cast<unsigned char const *>(
+		"HelloWorld"
+	)), *in_p(in);
 	uint32_t v1(0), v2(0);
 	uint32_t v3(0x15c1ba2b), v4(0x6b716e3a);
 	base86::decode(v1, in_p);
@@ -23,11 +25,14 @@ BOOST_AUTO_TEST_CASE(base85_0)
 	BOOST_CHECK_EQUAL(v1, v3);
 	BOOST_CHECK_EQUAL(v2, v4);
 
-	char out[11] = {0};
-	char *out_p(&out[0]);
+	unsigned char out[11] = {0};
+	unsigned char *out_p(&out[0]);
 	base86::encode(out_p, v3);
 	base86::encode(out_p, v4);
-	BOOST_CHECK(0 == std::strcmp(out, in));
+	BOOST_CHECK(0 == std::strcmp(
+		reinterpret_cast<char const *>(out),
+		reinterpret_cast<char const *>(in)
+	));
 }
 
 BOOST_AUTO_TEST_CASE(base85_1)
@@ -36,8 +41,8 @@ BOOST_AUTO_TEST_CASE(base85_1)
 	std::mt19937 gen(src());
 	std::uniform_int_distribution<uint32_t> dis;
 	constexpr static int count = 10000;
-	std::array<char, 6> buf;
-	char *p;
+	std::array<unsigned char, 6> buf;
+	unsigned char *p;
 	buf[5] = 0;
 
 	for (int c(0); c < count; ++c) {
