3b2b36278a4959cdc0813f549d4acff53573ef10
Alex Dubov <oakad@yahoo.com>
2014-06-17 04:51:05 +1000

    yesod::iterator: buffer_inserter<> output buffering iterator (2)
    Signed-off-by: Alex Dubov <oakad@yahoo.com>

--- yesod/yesod/iterator/buffer_inserter.hpp
+++ yesod/yesod/iterator/buffer_inserter.hpp
@@ -21,13 +21,12 @@ template <
 > {
 	static_assert(BlockSize > 1, "BlockSize > 1");
 
-	buffer_inserter(bool create = false)
-	{
-		if (!create)
-			return;
+	buffer_inserter()
+	{}
 
-		
-	}
+	buffer_inserter(bool create, Alloc const &a = Alloc())
+	: base_ptr(create ? allocate_counted(a) : counted_ptr<base_type>())
+	{}
 
 	buffer_inserter(buffer_inserter const &other)
 	: base_ptr(other.base_ptr)
@@ -49,18 +48,48 @@ template <
 		return *this;
 	}
 
-	buffer_inserter &operator=(ValueType const &value)
+	typename std::enable_if<
+		!std::is_pod<ValueType>::value, buffer_inserter &
+	>::type operator=(ValueType const &value)
 	{
 		auto n_pos(get_next_pos());
+		n_pos.cmd = COPY_VALUE;
+		n_pos.value_ptr = const_cast<ValueType *>(&value);
+		base_ptr.access_allocator(&n_pos);
 		return *this;
 	}
 
-	buffer_inserter &operator=(ValueType &&value)
+	typename std::enable_if<
+		!std::is_pod<ValueType>::value, buffer_inserter &
+	>::type operator=(ValueType &&value)
 	{
 		auto n_pos(get_next_pos());
+		n_pos.cmd = MOVE_VALUE;
+		n_pos.value_ptr = &value;
+		base_ptr.access_allocator(&n_pos);
 		return *this;
 	}
 
+	typename std::enable_if<
+		std::is_pod<ValueType>::value, buffer_inserter &
+	>::type operator=(ValueType const &value)
+	{
+		auto n_pos(get_next_pos());
+		*n_pos.node_ptr->items.ptr_at(n_pos.node_pos) = value;
+		return *this;
+	}
+
+	typename std::enable_if<
+		std::is_pod<ValueType>::value, buffer_inserter &
+	>::type operator=(ValueType &&value)
+	{
+		auto n_pos(get_next_pos());
+		*n_pos.node_ptr->items.ptr_at(
+			n_pos.node_pos
+		) = std::move(value);
+		return *this;
+	}
+	
 	buffer_inserter &operator*() const
 	{
 		return *this;
@@ -76,46 +105,147 @@ template <
 		return *this;
 	}
 
+	size_t size() const
+	{
+		size_t rv(0);
+		if (!base_ptr)
+			return rv;
+
+		auto last(base_ptr->node_ptr.load());
+		for (auto p(&base_ptr->first_node); p != last; p = p->next)
+			rv += BlockSize;
+
+		auto l_pos(last->pos.load());
+		if (l_pos)
+			rv += l_pos - 1;
+
+		return rv;
+	}
+
+	template <typename Iterator>
+	void copy(Iterator &first, Iterator last) const
+	{
+	}
+
+	template <typename Iterator>
+	void copy_n(Iterator &first, size_t n) const
+	{
+	}
+
 private:
+	struct all_valid_pred {
+		static bool test(ValueType const &v)
+		{
+			return true;
+		}
+	};
+
 	struct node_type {
-		node *prev;
+		node_type()
+		: next(nullptr), pos(0)
+		{}
+
+		node *next;
 		std::atomic<size_t> pos;
-		placement_array<ValueType, BlockSize, Alloc> items;
+		placement_array<
+			ValueType, BlockSize, Alloc, all_valid_pred
+		> items;
+	};
+
+	struct alloc_cmd {
+		enum {
+			NOP = 0,
+			ALLOC_NODE,
+			COPY_VALUE,
+			MOVE_VALUE
+		} cmd;
+		node_type *node_ptr;
+		size_t node_pos;
+		ValueType *value_ptr;
 	};
 
 	struct base_type {
-		std::atomic<node_type *> node_ptr;
-		node_type node;
+		base_type()
+		: node_ptr(&first_node), tail(&first_node)
+		{}
+
+		node_type *alloc_node()
+		{
+		}
+
+		template <typename Alloc1>
+		static node_type *alloc_node(Alloc1 &a)
+		{
+		}
+
+		template <typename Alloc1>
+		static void access_allocator(
+			Alloc1 &a, base_type const *self, void *data
+		)
+		{
+			auto cmd(reinterpret_cast<alloc_cmd *>(data));
+			switch (cmd->cmd) {
+			case alloc_cmd::ALLOC_NODE:
+				cmd->node_ptr = alloc_node(a);
+				cmd->node_ptr->pos = 1;
+				cmd->node_pos = 0;
+				self->tail->next = cmd->node_ptr;
+				self->tail = cmd->node_ptr;
+				self->node_ptr = cmd->node_ptr;
+				break;
+			case alloc_cmd::COPY_VALUE:
+				cmd->node_ptr->items.emplace_at(
+					cmd->node_pos, a,
+					*const_cast<ValueType const *>(
+						cmd->value_ptr
+					)
+				);
+				break;
+			case alloc_cmd::MOVE_VALUE:
+				cmd->node_ptr->items.emplace_at(
+					cmd->node_pos, a,
+					std::move(*cmd->value_ptr)
+				);
+				break;
+			default:
+				break;
+			}
+		}
+
+		mutable std::atomic<node_type *> node_ptr;
+		mutable node_type *tail;
+		mutable node_type first_node;
 	};
 
-	node_type *alloc_node()
+	alloc_cmd get_next_pos()
 	{
-		return nullptr;
-	}
-
-	std::pair<node_type *, size_t> get_next_pos()
-	{
-		std::pair<node_type *, size_t> rv(nullptr, 0);
+		alloc_cmd rv{
+			.cmd = alloc_cmd::NOP,
+			.node_ptr = nullptr,
+			.node_pos = 0
+		};
 
 		if (!base_ptr)
 			return rv;
 
+		/* It is somewhat unlikely that multiple threads will be
+		 * simultaneously writing to the same buffer, but such use
+		 * case is supported and the below code may need to be
+		 * improved if found inadequate.
+		 */
 		while (true) {
-			rv.first = base_ptr->node_ptr.load();
-			rv.second = node->head.fetch_add(1);
-			if (rv.second < BlockSize)
+			rv.node_ptr = base_ptr->node_ptr.load();
+			rv.node_pos = rv.node_ptr->pos.fetch_add(1);
+			if (rv.node_pos > BlockSize)
+				sched_yield();
+			else
 				break;
-			else if (rv.second == BlockSize) {
-				rv.first = alloc_node();
-				rv.first->prev = node;
-				rv.first->pos = 1;
-				rv.second = 0;
-				base_ptr->node_ptr = rv.first;
-				break;
-			}
-
-			std::atomic_thread_fence(std::memory_order_seq_cst);
 		};
+
+		if (rv.node_pos == BlockSize) {
+			rv.cmd = alloc_cmd::ALLOC_NODE;
+			base_ptr.access_allocator(&rv);
+		}
 
 		return rv;
 	}
