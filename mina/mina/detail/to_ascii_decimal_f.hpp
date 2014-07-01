ca2630a0f7cb8762689197288a5f4cf81e6652df
Alex Dubov <oakad@yahoo.com>
2014-07-02 05:32:17 +1000

    mina::to_ascii_decimal: to_ascii_decimal_f (3)
    Signed-off-by: Alex Dubov <oakad@yahoo.com>

--- mina/mina/detail/to_ascii_decimal_f.hpp
+++ mina/mina/detail/to_ascii_decimal_f.hpp
@@ -15,6 +15,7 @@
 #if !defined(UCPF_MINA_DETAIL_TO_ASCII_DECIMAL_F_20140624T2300)
 #define UCPF_MINA_DETAIL_TO_ASCII_DECIMAL_F_20140624T2300
 
+#include <mina/detail/bcd_to_ascii.hpp>
 #include <mina/detail/binary_pow_10.hpp>
 
 namespace ucpf { namespace mina { namespace detail {
@@ -90,6 +91,60 @@ struct to_ascii_decimal_f<double> {
 		}
 	};
 
+	static std::pair<uint32_t, int> pow_10_estimate(uint32_t v, int n_bits)
+	{
+		constexpr std::array<uint32_t, 11> pow_10 = {{
+			0, 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000,
+			100000000, 1000000000
+		}};
+
+		auto exp(((n_bits + 1) * 1233 >> 12) + 1);
+
+		if (v < pow_10[exp])
+			--exp;
+
+		return std::make_pair(pow_10[exp], exp);
+	}
+
+	static bool round_weed(
+		std::array<uint32_t, 3> const &v,
+		int digit_pos,
+		uint64_t upper_range, uint64_t unsafe_range,
+		uint64_t x_rem, uint64_t mult, uint64_t scale
+	)
+	{
+		auto range(std::make_pair(
+			upper_range - scale, upper_range + scale
+		));
+
+		while (
+			(x_rem < range.first)
+			&& ((unsafe_range - x_rem) >= mult)
+			&& (((x_rem + mult) < range.first) || (
+				(range.first - x_rem)
+				>= (x_rem + mult - range.first)
+			))
+		)
+		{
+			buffer[length - 1]--;
+			x_rem += mult;
+		}
+
+		if (
+			(x_rem < range.second)
+			&& ((unsafe_range - x_rem) >= mult)
+			&& (((x_rem + mult) < range.second) || (
+				(range.second - x_rem)
+				> (x_rem + mult - range.second)
+			)
+		)
+			return false;
+
+		return ((scale << 1) <= x_rem) && (
+			x_rem <= (unsafe_range - (mult << 2))
+		);
+	}
+
 	template <typename OutputIterator>
 	to_ascii_decimal_f(OutputIterator &&sink, double v)
 	{
@@ -104,9 +159,9 @@ struct to_ascii_decimal_f<double> {
 		auto exp_bd(binary_pow_10::lookup_exp_10<double>(
 			minimal_target_exp - (xv.exp + mantissa_size)
 		));
-		fp_type scale(exp_bd.m, exp_bd.exp_2);
-		auto s_xv(xv * scale);
-		auto s_bd(std::make_pair(bd.first * scale, bd.second * scale));
+		fp_type x_scale(exp_bd.m, exp_bd.exp_2);
+		auto s_xv(xv * x_scale);
+		auto s_bd(std::make_pair(bd.first * x_scale, bd.second * x_scale));
 
 		printf("s_xv %zx, %d\n", s_xv.m, s_xv.exp);
 		printf("s_bd min %zx, %d\n", s_bd.first.m, s_bd.first.exp);
@@ -120,7 +175,70 @@ struct to_ascii_decimal_f<double> {
 		fp_type x_one(uint64_t(1) << -s_xv.exp, s_xv.exp);
 		auto x_int(s_bd_outer.second.m >> -s_xv.exp);
 		auto x_frac(s_bd_outer.second.m & (x_one.m - 1));
-		
+		auto x_exp(pow_10_estimate(x_int, mantissa_size + x_one.exp));
+
+		std::array<uint32_t, 3> bv{0, 0, 0};
+		int dp(0);
+		uint64_t scale(1);
+		while (x_exp.second > 0) {
+			auto x_dig(x_int / x_exp.first);
+			bv[dp >> 3] |= x_dig << ((7 - (dp & 7)) << 2);
+			++dp;
+			x_int %= x_exp.first;
+			--x_exp.second;
+
+			auto x_rem((x_int << (-x_one.exp)) + x_frac);
+
+			if (x_rem < unsafe.m) {
+				printf("e_int\n");
+				if (!round_weed(
+					bv, dp - 1,
+					(s_bd_outer.second - s_xv).m,
+					unsafe.m, x_rem,
+					x_exp.first << (-x_one.exp), scale
+				)) {
+					printf("no match\n");
+				}
+				bcd_to_ascii(
+					std::forward<OutputIterator>(sink), bv
+				);
+				return;
+				/*
+				return RoundWeed(buffer, *length, DiyFp::Minus(too_high, w).f(),
+				unsafe_interval.f(), rest,
+				static_cast<uint64_t>(divisor) << -one.e(), scale);
+			*/
+				
+			}
+			x_exp.first /= 10;
+		}
+
+		while (true) {
+			x_frac *= 10;
+			scale *= 10;
+			unsafe.m *= 10;
+			auto x_dig(x_frac >> (-x_one.exp));
+			bv[dp >> 3] |= x_dig << ((7 - (dp & 7)) << 2);
+			++dp;
+			x_frac &= x_one.m - 1;
+			--x_exp.second;
+			if (x_frac < unsafe.m) {
+				printf("e_frac\n");
+				if (!round_weed(
+					bv, dp - 1,
+					(s_bd_outer.second - s_xv).m * scale,
+					unsafe.m, x_frac, x_one.m, scale
+				)) {
+					printf("no match\n");
+				}
+				bcd_to_ascii(
+					std::forward<OutputIterator>(sink), bv
+				);
+				return;
+				/*return RoundWeed(buffer, *length, DiyFp::Minus(too_high, w).f() * unit,
+                               unsafe_interval.f(), fractionals, one.f(), scale);*/
+			}
+		}
 	}
 };
 
